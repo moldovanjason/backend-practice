dfb7af2d04adb00080945464a27e69fdac6a58b7
Jason Moldovan <moldovanjason@gmail.com>
2021-03-08 10:34:50 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [initialData, setInitialData] = useState([{
+
+  }])
+
+  useEffect( () => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(data => setInitialData(data))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{initialData.title}</h1>
+      <form>
+        <div className="firstName">First Name</div>
+        <input type="text"/>
+        <div className="lastName">Last Name</div>
+        <input type="text"/>
+        <div className="email">Email</div>
+        <input type="text"/>
+        <div className="password">Password</div>
+        <input type="password"/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
